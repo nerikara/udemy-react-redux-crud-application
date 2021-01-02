27f1df544111c9c92661b507130e30738327c695
nerikara <nerikara4chimi@gmail.com>
2021-01-02 20:03:01 +0900

    actionとaction createrを実装

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT';
+export const DEDCREMENT = 'DECREMENT';
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
